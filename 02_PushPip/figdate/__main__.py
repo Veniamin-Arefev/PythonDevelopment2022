728d78b748a8b0b7ee52b27b7e02379aff0e2fbe
Veniamin_Arefev <veniamin.arefev@mail.ru>
2022-02-17 13:58:30 +0300

    Done 1.2

--- 02_PushPip/figdate/__main__.py
+++ 02_PushPip/figdate/__main__.py
@@ -4,6 +4,11 @@ import sys
 from figdate import *
 
 locale.setlocale(locale.LC_ALL, '')
+# locale.setlocale(locale.LC_ALL, 'ru_RU')
+
+print(locale.normalize('ru'))
 # locale.setlocale(locale.LC_ALL, 'Russian_Russia')
 if 3 >= len(sys.argv) >= 1:
     print(date(*sys.argv[1:]))
+else:
+    print("Please run with 3 or less arguments")
